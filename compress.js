5df2b7ae6fd03c6fdab5638d975415ec13123bad
yunwisdom <qq695926920@163.com>
2019-07-07 16:46:44 +0800

    添加调试配置

--- compress.js
+++ compress.js
@@ -9,83 +9,80 @@ global.recomputingkey = false;
 var events = require("events");
 global.eventCompress = new events.EventEmitter();
 
-function tinifyCompress(srcfile, desfile){
+function tinifyCompress(srcfile, desfile) {
 
-	fs.readFile(srcfile, function(err, sourceData) {
-	  if (err) throw err;
+    fs.readFile(srcfile, function(err, sourceData) {
+        if (err) throw err;
 
-	  console.log('compress started.'+srcfile);
+        console.log('compress started.' + srcfile);
 
-	  tinify.fromBuffer(sourceData).toBuffer(function(err, resultData) {
-	    // if (err) throw err;
-	    // fs.writeFileSync(desfile, resultData);
+        tinify.fromBuffer(sourceData).toBuffer(function(err, resultData) {
+            // if (err) throw err;
+            // fs.writeFileSync(desfile, resultData);
 
-	    if (err instanceof tinify.ConnectionError ||
-	    	err instanceof tinify.ServerError){
-	    	console.log('compress failed.'+srcfile+', recompress.');
-			tinifyCompress(srcfile, desfile);
-			return;
-		}
-		else if(err instanceof tinify.ClientError ||
-				err instanceof tinify.AccountError){
+            if (err instanceof tinify.ConnectionError ||
+                err instanceof tinify.ServerError) {
+                console.log('compress failed.' + srcfile + ', recompress.');
+                tinifyCompress(srcfile, desfile);
+                return;
+            } else if (err instanceof tinify.ClientError ||
+                err instanceof tinify.AccountError) {
 
-			if (err.message.indexOf('Your monthly limit has been exceeded') >= 0){
-				/*该账户数目超过，换下一个key重试*/
-				//查看是否还有可用的账户
-				if (global.recomputingkey === false){
-				//尚未在本次换过key
-					global.recomputingkey = true;
-					global.keyindex++;
-					if (global.keyindex < global.key.length){
-						console.log('use no'+global.keyindex+' account key.');
-						tinify.key = global.key[global.keyindex] ;
-						tinifyCompress(srcfile, desfile);
-					}
-					else{
-						console.log('no valide account.');
-						process.exit();
-					}
-				}
-				else{
-				//已经在本次换过key
-					tinifyCompress(srcfile, desfile);
-				}
-				return;
-			}
-			
-			console.log('comressed failed:'+(counter+1)+'/'+global.varA+'('+desfile+')');
-			tinifyCounter(1);
-			return;
-		}
+                if (err.message.indexOf('Your monthly limit has been exceeded') >= 0) {
+                    /*该账户数目超过，换下一个key重试*/
+                    //查看是否还有可用的账户
+                    if (global.recomputingkey === false) {
+                        //尚未在本次换过key
+                        global.recomputingkey = true;
+                        global.keyindex++;
+                        if (global.keyindex < global.key.length) {
+                            console.log('use no' + global.keyindex + ' account key.');
+                            tinify.key = global.key[global.keyindex];
+                            tinifyCompress(srcfile, desfile);
+                        } else {
+                            console.log('no valide account.');
+                            process.exit();
+                        }
+                    } else {
+                        //已经在本次换过key
+                        tinifyCompress(srcfile, desfile);
+                    }
+                    return;
+                }
 
-		fs.writeFileSync(desfile, resultData);
-		console.log('comressed:'+(counter+1)+'/'+global.varA+'('+desfile+')');
-		tinifyCounter(0);
+                console.log('comressed failed:' + (counter + 1) + '/' + global.varA + '(' + desfile + ')');
+                tinifyCounter(1);
+                return;
+            }
 
-	  });
-	});
+            fs.writeFileSync(desfile, resultData);
+            console.log('comressed:' + (counter + 1) + '/' + global.varA + '(' + desfile + ')');
+            tinifyCounter(0);
+
+        });
+    });
 }
 
-function tinifyCounter(type){
-	switch(type){
-		case 0:
-			successcounter++;
-			break;
-		case 1:
-			failedcounter++;
-			break;
-		default:
-			break;
-	}
-	counter++;
+function tinifyCounter(type) {
+    switch (type) {
+        case 0:
+            successcounter++;
+            break;
+        case 1:
+            failedcounter++;
+            break;
+        default:
+            break;
+    }
+    counter++;
 
-	global.eventCompress.emit('compressFinish');
+    global.eventCompress.emit('compressFinish');
 
-	if (counter === global.varA){
-		global.eventCompress.emit('FinishAll');
-		var result = "result  {success:"+successcounter+"/"+counter+",failed:"+failedcounter+"/"+counter+"}";
-		console.log(result);
-	}
+    if (counter === global.varA) {
+        global.eventCompress.emit('FinishAll');
+        var result = "result  {success:" + successcounter + "/" + counter + ",failed:" + failedcounter + "/" + counter + "}";
+        console.log(result);
+    }
 }
 
-exports.tinifyCompress  = tinifyCompress;
+exports.tinifyCompress = tinifyCompress;
